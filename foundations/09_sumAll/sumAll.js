c9a6db360d689a84bcd3a3ddbd56695cd5c1eb32
rizanne-f <6rai21@gmail.com>
2026-07-16 15:29:14 +0800

    Add solution for sumAll exercise using isInteger method, array destructuring, and for loop.

--- foundations/09_sumAll/sumAll.js
+++ foundations/09_sumAll/sumAll.js
@@ -1,5 +1,14 @@
-const sumAll = function() {
-
+const sumAll = function(num1, num2) {
+    if (!Number.isInteger(num1) || !Number.isInteger(num2)) return 'ERROR'
+    if (num1 < 0 || num2 < 0) return 'ERROR'
+    
+    let arr = [num1, num2].sort()
+    let sum = 0
+    for (let i = arr[0]; i <= arr[1]; i++) {
+        sum += i
+    }
+    
+    return sum
 };
 
 // Do not edit below this line
