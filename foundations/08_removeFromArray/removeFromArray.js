e3d5ec42b3dada470f124dc5350af88693a11bc7
rizanne-f <6rai21@gmail.com>
2026-07-16 15:01:46 +0800

    Add solution for removeFromArray exercise using filter and includes array methods

--- foundations/08_removeFromArray/removeFromArray.js
+++ foundations/08_removeFromArray/removeFromArray.js
@@ -1,4 +1,9 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr, ...val) {
+    return arr.filter(n => !val.includes(n))
+    
+    // a more roundabout solution
+    // val.map(n => arr = arr.filter(num => num !== n))
+    // return arr
 };
 
 // Do not edit below this line
