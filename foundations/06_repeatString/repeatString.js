642b43ff8603781f3ace2c10feadc0ae5cea1b88
rizanne-f <6rai21@gmail.com>
2026-07-16 14:33:27 +0800

    Add answer for repeatString exercise using while loop

--- foundations/06_repeatString/repeatString.js
+++ foundations/06_repeatString/repeatString.js
@@ -1,5 +1,14 @@
-const repeatString = function() {
+const repeatString = function(str, num) {
+    let result = "";
 
+    if (num < 0) return "ERROR"; 
+
+    while(num > 0) {
+        result += str;
+        num--;
+    }
+
+    return result;
 };
 
 // Do not edit below this line
