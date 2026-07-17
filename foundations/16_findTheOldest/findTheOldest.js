8b8a9c93bca1d5e5ae8024cee24b612540480bb7
rizanne-f <6rai21@gmail.com>
2026-07-17 20:04:32 +0800

    Add solution for findTheOldest using map and sort

--- foundations/16_findTheOldest/findTheOldest.js
+++ foundations/16_findTheOldest/findTheOldest.js
@@ -1,5 +1,7 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(arr) {
+    arr = arr.map(person => "yearOfDeath" in person ? person : {...person, yearOfDeath: new Date().getFullYear})
+    let sorted = arr.sort((a, b) => (a.yearOfDeath - a.yearOfBirth) - (b.yearOfDeath - b.yearOfBirth))
+    return sorted[sorted.length-1]
 };
 
 // Do not edit below this line
