d4d940ef3c15c41ccb7adf7322fa6c266284ca40
rizanne-f <6rai21@gmail.com>
2026-07-17 18:28:57 +0800

    add solution for palindrome exercise

--- foundations/13_palindromes/palindromes.js
+++ foundations/13_palindromes/palindromes.js
@@ -1,5 +1,7 @@
-const palindromes = function () {
-
+const palindromes = function (str) {
+    let string = str.replace(/[^a-zA-Z0-9]/g, "").toLowerCase();
+    let reversedString = string.split("").reverse().join("");
+    return string === reversedString;
 };
 
 // Do not edit below this line
